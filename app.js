9977bfb614f70090db1c51c34d2a62601665b6df
rjawesome <aonedtop@gmail.com>
2020-06-18 16:19:24 -0700

    added dynamic rendering

--- app.js
+++ app.js
@@ -3,8 +3,23 @@ var app = express();
 
 var serv = require('http').Server(app);
 
+app.engine('html', require('ejs').renderFile);
+
+var links1 = ['https://echoar-storage.s3.us-east-2.amazonaws.com/0_model_samples/d686a655-e800-430d-bfd2-e38cdfb0c9e9.glb','https://echoar-storage.s3.us-east-2.amazonaws.com/0_model_samples/6af76ce2-2f57-4ed0-82d8-42652f0eddbe.glb']
+var links2 = ['https://echoar-storage.s3.us-east-2.amazonaws.com/0_model_samples/d686a655-e800-430d-bfd2-e38cdfb0c9e9.usdz','https://echoar-storage.s3.us-east-2.amazonaws.com/0_model_samples/6af76ce2-2f57-4ed0-82d8-42652f0eddbe.usdz']
+var names = ['skyscraper','fox']
+
 app.get('/',function(req,res) {
-        res.sendFile('./client/index.html', { root: __dirname })
+    res.sendFile('./client/index.html', { root: __dirname })
+});
+
+app.get('/template',function(req,res) {
+	var index = 0;
+	res.render('template.html', {root: __dirname, name: names[index], link1: links1[index], link2: links2[index]})
+});
+
+app.get('/fox',function(req,res) {
+    res.sendFile('./client/fox.html', { root: __dirname })
 });
 
 app.use('/client',express.static('./client', { root: __dirname }));
