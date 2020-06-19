0ce062ee229d8524ed088d56f3777eaeee8a3bd2
rjawesome <aonedtop@gmail.com>
2020-06-19 14:39:09 -0700

    deleted logout.js

--- js/logout.js
+++
@@ -1,8 +0,0 @@
-const auth = firebase.auth();
-    document.getElementById("logout").onclick = function () {
-      console.log("log out")
-      auth.signOut().then(() => {
-        window.location.pathname = '/login'
-        console.log("logged out")
-      })
-    }
