f86c6fba6373917ef07b4ca7410122f5c01c211e
Aadit Gupta <spacegiant21@gmail.com>
2020-06-19 18:02:08 -0700

    points and more 3d stuff

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var objects = [
     link2:
       "https://echoar-storage.s3.us-east-2.amazonaws.com/0_model_samples/d686a655-e800-430d-bfd2-e38cdfb0c9e9.usdz",
     name: "Skyscraper",
+    letter: "S",
   },
   {
     link1:
@@ -19,6 +20,7 @@ var objects = [
     link2:
       "https://echoar-storage.s3.us-east-2.amazonaws.com/0_model_samples/6af76ce2-2f57-4ed0-82d8-42652f0eddbe.usdz",
     name: "Fox",
+    letter: "F",
   },
   {
     link1:
@@ -26,6 +28,7 @@ var objects = [
     link2:
       "https://echoar-storage.s3.us-east-2.amazonaws.com/polished-math-8279/6bbebac8-57d6-4bf1-8d82-e2f2ee992f25.usdz",
     name: "Apple",
+    letter: "A",
   },
   {
     link1:
@@ -33,6 +36,47 @@ var objects = [
     link2:
       "https://echoar-storage.s3.us-east-2.amazonaws.com/polished-math-8279/d1564af7-9db9-441a-850c-25e4f1640db6.usdz",
     name: "Basketball",
+    letter: "B",
+  },
+  {
+    link1:
+      "https://echoar-storage.s3.us-east-2.amazonaws.com/polished-math-8279/cff43987-5cfc-4824-be21-f79163d2932f.glb",
+    link2:
+      "https://echoar-storage.s3.us-east-2.amazonaws.com/polished-math-8279/8781b47b-6082-4c31-91b1-f770243b42f7.usdz",
+    name: "Cat",
+    letter: "C",
+  },
+  {
+    link1:
+      "https://echoar-storage.s3.us-east-2.amazonaws.com/polished-math-8279/d6239259-bf96-4883-b139-c9ddc004bac9.glb",
+    link2:
+      "https://echoar-storage.s3.us-east-2.amazonaws.com/polished-math-8279/00172d72-b03e-4849-b1a8-318a933c56e0.usdz",
+    name: "Dog",
+    letter: "D",
+  },
+  {
+    link1:
+      "https://echoar-storage.s3.us-east-2.amazonaws.com/polished-math-8279/80aa0107-0da1-413f-9925-1b11482d4ed2.glb",
+    link2:
+      "https://echoar-storage.s3.us-east-2.amazonaws.com/polished-math-8279/d352a708-a744-4608-869c-8483938b9838.usdz",
+    name: "Elephant",
+    letter: "E",
+  },
+  {
+    link1:
+      "https://echoar-storage.s3.us-east-2.amazonaws.com/polished-math-8279/93cbf9f9-614f-4a3a-9703-dd68f9a0a214.glb",
+    link2:
+      "https://echoar-storage.s3.us-east-2.amazonaws.com/polished-math-8279/8350bd0d-7950-4b49-9079-6e6546397058.usdz",
+    name: "Giraffe",
+    letter: "G",
+  },
+  {
+    link1:
+      "https://echoar-storage.s3.us-east-2.amazonaws.com/polished-math-8279/aedccc02-73f2-477f-b4f4-fab8e0a69c9d.glb",
+    link2:
+      "https://echoar-storage.s3.us-east-2.amazonaws.com/polished-math-8279/60f3d3c2-0beb-43af-8f46-304e9b175144.usdz",
+    name: "House",
+    letter: "H",
   },
 ];
 
@@ -43,6 +87,7 @@ app.get("/", function (req, res) {
     name: object.name,
     link1: object.link1,
     link2: object.link2,
+    letter: object.letter,
   });
 });
 
--- js/template.js
+++ js/template.js
@@ -2,29 +2,43 @@ var current_user = null;
 firebase.auth().onAuthStateChanged((user) => {
   if (!user) {
     window.location.pathname = "/login";
-  }
-  else {
+  } else {
     current_user = user;
   }
 });
 
-function quiz () {
-  if (current_user === null) {
-    return
-  }
-  var value = '';
-  if (value.toUpperCase() === answer.toUpperCase()) {
-    current_user.updateProfile({
-      displayName: parseInt(current_user.displayName)+10,
-    }).then(() => {
-      alert("Correct Answer")
-    })
-  }
-  else {
-    alert("Incorrect answer! The answer was " + answer)
-  }
-}
+var form = document.getElementById("form");
 
-function logout () {
-  firebase.auth().signOut()
+form.onsubmit = (e) => {
+  e.preventDefault();
+  if (current_user === null) {
+    return;
+  }
+  var value = form["word"].value;
+  if (
+    letter.toUpperCase() + value.toUpperCase() === answer.toUpperCase() ||
+    value.toUpperCase() === answer.toUpperCase()
+  ) {
+    current_user
+      .updateProfile({
+        displayName: parseInt(current_user.displayName) + 10,
+      })
+      .then(() => {
+        if (confirm("Correct Answer")) {
+          window.location.pathname = "/";
+        } else {
+          window.location.pathname = "/";
+        }
+      });
+  } else {
+    if (confirm("Incorrect answer. The correct answer was "+answer)) {
+      window.location.pathname = "/";
+    } else {
+      window.location.pathname = "/";
+    }
+  }
+};
+
+function logout() {
+  firebase.auth().signOut();
 }
